36e0dc1f10b7482de277e605bccda4a532139af7
admin <admin@hp>
2023-07-21 11:50:01 +0530

    first commit

---
+++ N06_CoreJava/src/dayone/inputstring.java
@@ -0,0 +1,17 @@
+//program to demonstrate on user-input value
+package dayone;
+
+import java.util.Scanner;
+
+public class inputstring 
+{
+
+	public static void main(String[] args) {
+	Scanner s= new Scanner(System.in);
+	System.out.println("enter a string:");
+	String a=s.nextLine();
+	System.out.println(" string is "+a);
+
+	}
+
+}
